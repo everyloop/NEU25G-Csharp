6885153218c209da04d2cc9f62f5960d01badd65
Fredrik Johansson <fredrik@everyloop.com>
2025-09-03 11:15:11 +0200

    Code cleanup

--- Code-alongs/L001_Introduktion/Program.cs
+++ Code-alongs/L001_Introduktion/Program.cs
@@ -19,19 +19,20 @@ if (true)
 // Keywords - (Av C#-språket) reserverad ord
 // t.ex: if, for, true, while
 
+
 // Case
-//-UPPERCASE
-//- lowercase
-//- camelCase(förstaOrdetBörjarMedLitenBokstavSedanRestenBörjarPåStor)
-//- PascalCase(AllaOrdBörjarPåStorBokstav)
-//- snake_case(bara_små_bokstäver_ord_avskiljda_med_underscore) < -Används ej i C#
+// -UPPERCASE
+// - lowercase
+// - camelCase(förstaOrdetBörjarMedLitenBokstavSedanRestenBörjarPåStor)
+// - PascalCase(AllaOrdBörjarPåStorBokstav)
+// - snake_case(bara_små_bokstäver_ord_avskiljda_med_underscore) < -Används ej i C#
 // - kebab-case (bara_små_bokstäver_ord_avskiljda_med_bindesträck)   <- Används ej i C#
 
 // Språket C# är case sensitive, till skillnad från vissa andra språk som är case insensitve
 
 
 // Identifiers - Namn som man ger på variabler, funktioner, klasser etc.
-//-Kan innehålla bokstäver, nummer och underscore (_), inte övriga tecken eller space.
+// - Kan innehålla bokstäver, nummer och underscore (_), inte övriga tecken eller space.
 // - Måste börja med bokstav eller underscore (inte siffra)
 // - Man kan inte använda reserverade ord (keywords)
 // - variabler och parameterar skrivs i camelCase, alla andra identifiers (t.ex klasser & metoder) använder PascalCase. (enligt konvention)
