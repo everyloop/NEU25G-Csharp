428edc428695f260230a51a12a26b76160a30ec8
Fredrik Johansson <fredrik@everyloop.com>
2025-09-03 12:11:33 +0200

    Add definitions and examples for: - Implicitly typed variables (var) - Scope

--- Code-alongs/L002_Variabler/Program.cs
+++ Code-alongs/L002_Variabler/Program.cs
@@ -12,6 +12,8 @@ bool isEnabled = true;
 // Literal values
 // t.ex 3, 3.45, 'A', "Fredrik", true
 
+// literal values kan använda sig av suffix för att avgöra typen på det "hårdkodade" värdet:
+// T.ex: 1.0f är float-värde, medan 1.0 är double-värde.
 
 // Data types (datatyper) - Anger vilken typ av data vi lagrar i t.ex en variabel.
 
@@ -27,7 +29,53 @@ bool isEnabled = true;
 Console.WriteLine(y + 3);
 // Console.WriteLine(isEnabled + 3);  <= Otillåten operation mellan bool och int.
 
-byte myByte = 300;
+// byte myByte = 300;
 short myShort = 300;
-byte myOtherByte = -2;
+// byte myOtherByte = -2;
 sbyte mySignedByte = -2;
+
+
+// Implicit (underförstått)  & Explicit (uttryckligen) typade variabler.
+
+// Explicit typad variabel - Vi har uttryckligen i koden angett att datatype på myInt är 'int'.
+int myInt; // Likvärdig med Int32 myInt;  Int32 är namnet på .NET-typen, och int är C-sharps alias för Int32.
+
+// Implicit typad variabel - Med keyword var så avgör kompilatorn datatyp.
+var myVariable = 5.2;
+myVariable = 5.3;
+
+
+// Scope - En variabels (ett objekts) livstid (lifetime).
+// Class-scope, Method-scope & Block-scope.
+
+
+bool myBool = false;
+
+if (isEnabled)
+{
+    bool myOtherBool = true;
+    
+    if (1 == 1)
+    {
+        myBool = true;
+        bool myThirdBool = true;
+        Console.WriteLine(myBool);
+        Console.WriteLine(myOtherBool);
+        Console.WriteLine(myThirdBool);
+
+    }
+
+    Console.WriteLine(myBool);
+    Console.WriteLine(myOtherBool);
+    Console.WriteLine(myThirdBool);
+}
+
+Console.WriteLine(myBool);
+Console.WriteLine(myOtherBool);
+Console.WriteLine(myThirdBool);
+
+
+
+
+
+
