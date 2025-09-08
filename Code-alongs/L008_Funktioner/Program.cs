8e57eafdbf20964dc7716a4fae1a25ca799b611c
Fredrik Johansson <fredrik@everyloop.com>
2025-09-08 11:56:18 +0200

    Update

--- Code-alongs/L008_Funktioner/Program.cs
+++ Code-alongs/L008_Funktioner/Program.cs
@@ -50,9 +50,9 @@ MyFunction("Min text", 3);
 MyFunction("Lorem Ipsum", 5, 2);
 MyFunction("Lorem Ipsum", numberOfExclamationMarks: 5);
 
-Console.Write("Skriv text: ");
-string text = Console.ReadLine();
-MyFunction(text, 10);
+//Console.Write("Skriv text: ");
+//string text = Console.ReadLine();
+//MyFunction(text, 10);
 
 // Uppgift1: Skriv en funktion som tar två heltal in och skriver ut summan av dessa på skärmen.
 // Uppgift2: Skriv en funktion som tar två heltal t.ex 2 & 4 och skriver ut en sträng "2 * 4 = 8"
@@ -75,3 +75,38 @@ Addition(3, 4);
 Uppgift2(3, 4);
 
 Addition(5, 3);
+
+Console.WriteLine();
+
+// En funktion kan returnera 0 eller 1 objekt.
+// Man anger datatypen på objektet som returneras före funktionsnamnet.
+// Alternativt använd "void" om funktionen inte returnerar ett värde.
+// Koden (i funktionen) måste ALLTID returnera en matchande datatyp.
+static string AddNumbers(int a, int b)
+{
+    if (a < 10)
+    {
+        return $"{a} + {b} = {a+b}";
+    }
+    else
+    {
+        return "Hello world!";
+    }
+}
+
+string x = AddNumbers(13, 5);
+
+Console.WriteLine(x);
+
+Console.WriteLine(AddNumbers(3, 7));
+
+// Uppgift3: Skriv en funktion som tar en char och en int
+// och returnerar en string med tecknet upprepat x antal gånger.
+// Skriv sedan ut resultat på skärmen.
+// Ex. GetMultipleChars('A', 5);  => "AAAAA"
+
+// Uppgift4: Skriv en funktion som tar två heltal 'multiple' och 'length'
+// och returnerar en int[] med längden 'length' där varje element är multiplar av 'multiple'
+// i tur och ording:
+// Ex. GetMultiples(3, 5); => int[5] { 3, 6, 9, 12, 15 }
+// Spara värdet som funktionen returnerar i en variabel, och skriv sedan ut alla värden på skärmen.
