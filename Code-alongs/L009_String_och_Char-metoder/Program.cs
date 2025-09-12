7be1df01dbccbbe441ff3afe5cb10996a7007bf1
Fredrik Johansson <fredrik@everyloop.com>
2025-09-12 12:03:22 +0200

    Add L009 & L010

---
+++ Code-alongs/L009_String_och_Char-metoder/Program.cs
@@ -0,0 +1,86 @@
+﻿
+string text = string.Empty; // Skapa en tom sträng. 
+text = "Hello world!";
+
+int textLength = text.Length;
+
+Console.WriteLine($"text = \"{text}\"\n" );
+Console.WriteLine($"text.Length => {textLength}\n");
+
+Console.WriteLine($"text.ToLower() => {text.ToLower()}");
+Console.WriteLine($"text.ToUpper() => {text.ToUpper()}");
+Console.WriteLine($"text.StartsWith(\"Hello\") => {text.StartsWith("Hello")}");
+Console.WriteLine($"text.StartsWith(\"hello\") => {text.StartsWith("hello")}");
+Console.WriteLine($"text.EndsWith('!') => {text.EndsWith('!')}");
+Console.WriteLine($"text.Contains(\"orl\") => {text.Contains("orl")}");
+Console.WriteLine($"text.Replace('l', '-') => {text.Replace('l', '-')}");
+Console.WriteLine($"text.Remove(7) => {text.Remove(7)}");
+Console.WriteLine($"text.Remove(7, 3) => {text.Remove(7, 3)}");
+Console.WriteLine($"text.Substring(7) => {text.Substring(7)}");
+Console.WriteLine($"text.Substring(7, 3) => {text.Substring(7, 3)}");
+Console.WriteLine($"text.Insert(7, \"<-->\") => {text.Insert(7, "<-->")}");
+Console.WriteLine($"text.IndexOf('o') => {text.IndexOf('o')}");
+Console.WriteLine($"text.LastIndexOf('o') => {text.LastIndexOf('o')}");
+
+text = "       Hello world!      ";
+Console.WriteLine($"\ntext = \"{text}\"\n");
+
+Console.WriteLine($"text.Trim()) => \"{text.Trim()}\"");
+Console.WriteLine($"text.TrimStart()) => \"{text.TrimStart()}\"");
+Console.WriteLine($"text.TrimEnd()) => \"{text.TrimEnd()}\"");
+
+text = "This is a sample text";
+Console.WriteLine($"\ntext = \"{text}\"\n");
+Console.WriteLine("text.Split(' ')");
+
+string[] words = text.Split(' ');
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+Console.WriteLine($"\nstring.Join('-', words) => \"{string.Join('-', words)}\"");
+Console.WriteLine($"string.Join(\", \", words) => \"{string.Join(", ", words)}\"");
+Console.WriteLine($"string.Join(\" \", words) => \"{string.Join(" ", words)}\"");
+
+Console.WriteLine("\n\n *** Char methods ***\n");
+
+char myChar = 'A';
+Console.WriteLine($"myChar = '{myChar}'\n");
+
+Console.WriteLine($"char.IsLower(myChar) => {char.IsLower(myChar)}");
+Console.WriteLine($"char.IsUpper(myChar) => {char.IsUpper(myChar)}\n");
+
+Console.WriteLine($"char.ToLower(myChar) => {char.ToLower(myChar)}");
+Console.WriteLine($"char.ToUpper(myChar) => {char.ToUpper(myChar)}\n");
+
+Console.WriteLine($"char.IsDigit('R') => {char.IsDigit('R')}");
+Console.WriteLine($"char.IsDigit('7') => {char.IsDigit('7')}");
+Console.WriteLine($"char.IsDigit('%') => {char.IsDigit('%')}\n");
+
+Console.WriteLine($"char.IsLetter('R') => {char.IsLetter('R')}");
+Console.WriteLine($"char.IsLetter('7') => {char.IsLetter('7')}");
+Console.WriteLine($"char.IsLetter('%') => {char.IsLetter('%')}\n");
+
+Console.WriteLine($"char.IsLetterOrDigit('R') => {char.IsLetterOrDigit('R')}");
+Console.WriteLine($"char.IsLetterOrDigit('7') => {char.IsLetterOrDigit('7')}");
+Console.WriteLine($"char.IsLetterOrDigit('%') => {char.IsLetterOrDigit('%')}\n");
+
+Console.WriteLine($"char.IsWhiteSpace(' ') => {char.IsWhiteSpace(' ')}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Code-alongs/L010_Math_Random_Console/Program.cs
@@ -0,0 +1,60 @@
+﻿
+Console.WriteLine("*** Math ***\n");
+
+Console.WriteLine($"Math.PI => {Math.PI}");
+Console.WriteLine($"Math.E => {Math.E}\n");
+
+// Absolutbelopp: Gör talet positivt.
+Console.WriteLine($"Math.Abs(3) => {Math.Abs(3)}");
+Console.WriteLine($"Math.Abs(-3) => {Math.Abs(-3)}\n");
+
+// Avrundningsfunktioner
+Console.WriteLine($"Math.Floor(2.731) => {Math.Floor(2.731)}");
+Console.WriteLine($"Math.Ceiling(2.731) => {Math.Ceiling(2.731)}");
+Console.WriteLine($"Math.Round(2.731) => {Math.Round(2.731)}");
+Console.WriteLine($"Math.Truncate(2.731) => {Math.Truncate(2.731)}\n");
+
+Console.WriteLine($"Math.Floor(-2.731) => {Math.Floor(-2.731)}");
+Console.WriteLine($"Math.Truncate(-2.731) => {Math.Truncate(-2.731)}\n");
+
+Console.WriteLine($"Math.Round(2.731, 1) => {Math.Round(2.731, 1)}");
+Console.WriteLine($"Math.Round(2.731, 2) => {Math.Round(2.731, 2)}\n");
+
+// Min, Max, Clamp
+Console.WriteLine($"Math.Min(5, 12) => {Math.Min(5, 12)}");
+Console.WriteLine($"Math.Max(5, 12) => {Math.Max(5, 12)}\n");
+
+Console.WriteLine($"Math.Clamp(2, 5, 12) => {Math.Clamp(2, 5, 12)}");
+Console.WriteLine($"Math.Clamp(7, 5, 12) => {Math.Clamp(7, 5, 12)}");
+Console.WriteLine($"Math.Clamp(18, 5, 12) => {Math.Clamp(18, 5, 12)}\n");
+
+int userInput = 103;
+int clampedValue = Math.Clamp(userInput, 0, 100);
+
+// Upphöjt & Kvadratroot
+Console.WriteLine($"Math.Pow(10, 3) => {Math.Pow(10, 3)}");
+Console.WriteLine($"Math.Sqrt(9) => {Math.Sqrt(9)}\n");
+
+Console.WriteLine("*** Math ***\n");
+
+Console.WriteLine($"int.MinValue => {int.MinValue}");
+Console.WriteLine($"int.MaxValue => {int.MaxValue}\n");
+
+Console.WriteLine("Random myRandom = new Random();");
+Random myRandom = new Random();
+
+Console.WriteLine($"myRandom.Next() => {myRandom.Next()}\n");
+
+for (int i = 0; i < 20; i++)
+{
+   Console.WriteLine($"myRandom.Next() => {myRandom.Next(1, 7)}");
+}
+
+double range = 50.0;
+double offset = -25;
+
+Console.WriteLine($"\nmyRandom.NextDouble() => {myRandom.NextDouble() * range + offset}");
+
+
+
+
