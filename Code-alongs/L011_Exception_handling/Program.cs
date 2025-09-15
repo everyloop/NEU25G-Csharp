6dacc773160cf3769ae4bd8337c01960579f432e
Fredrik Johansson <fredrik@everyloop.com>
2025-09-15 13:14:20 +0200

    Add more examples

--- Code-alongs/L011_Exception_handling/Program.cs
+++ Code-alongs/L011_Exception_handling/Program.cs
@@ -20,27 +20,80 @@
 //int y = 1 / x;
 
 
+static void TryCatchExample()
+{
+    try
+    {
+        //try
+        //{
+        //    int x = 0;
+        //    int y = 1 / x;
+        //    Byte.Parse("hej");
+        //}
+        //catch (FormatException ex)
+        //{
+        //    Console.WriteLine("Parsing went wrong!");
+        //}
+
+        //int x = 0;
+        //int y = 1 / x;
+        Console.WriteLine("Start of try block.");
+        int[] myArray = new int[10];
+        myArray[15] = 3;
+        Console.WriteLine("End of try block.");
+    }
+    catch (IndexOutOfRangeException ex)
+    {
+        Console.WriteLine("Array indexing went wrong!");
+        throw;
+    }
+    //catch (Exception ex)
+    //{
+    //    Console.WriteLine($"Something went wrong: {ex.Message}");
+    //    Console.WriteLine(ex.StackTrace);
+    //}
+    finally
+    {
+        //int[] myOtherArray = new int[10];
+        //myOtherArray[15] = 3;
+        Console.WriteLine("This always runs at the end of try/catch.");
+    }
+
+}
+
+
+Console.WriteLine("Start program!");
+
 try
 {
-    //Byte.Parse("hej");
-    int x = 0;
-    int y = 1 / x;
-    Console.WriteLine("Start of try block.");
-    int[] myArray = new int[10];
-    myArray[15] = 3;
-    Console.WriteLine("End of try block.");
+    Console.WriteLine("Before function call.");
+    TryCatchExample();
+    //int userAge = -50;
+    //Console.WriteLine($"IsLegalAge({userAge}) => {IsLegalAge(userAge)}");
+    Console.WriteLine("After function call.");
 }
-catch (IndexOutOfRangeException ex)
+catch (DivideByZeroException ex)
 {
-    Console.WriteLine("Array indexing went wrong!");
+    Console.WriteLine("ERROR: Tried to divide by zero.");
 }
-catch (FormatException ex)
+catch (ArgumentOutOfRangeException ex)
 {
-    Console.WriteLine("Parsing went wrong!");
+    Console.WriteLine("Not valid age!");
 }
 catch (Exception ex)
 {
-    Console.WriteLine("Catch all uncatched exceptions.");
+    Console.WriteLine($"Nothing else catched this one: {ex.Message}");
 }
 
-Console.WriteLine("The end!");
+
+ Console.WriteLine("The end!");
+
+
+static bool IsLegalAge(int age)
+{
+    if (age < 0)
+    {
+        throw new ArgumentOutOfRangeException();
+    }
+    return age >= 18;
+}
