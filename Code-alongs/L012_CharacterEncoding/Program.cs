4d0616e42fd728301e7e961c3f0c89d64f5494c4
Fredrik Johansson <fredrik@everyloop.com>
2025-09-17 14:15:49 +0200

    Add L012

---
+++ Code-alongs/L012_CharacterEncoding/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+char myChar = '\u0578';
+
+Console.WriteLine(myChar);
+Console.WriteLine((int)myChar);
+
+Console.WriteLine();
+
+for (int i = 65; i <= 90; i++)
+{
+    Console.Write((char)i);
+}
+
+Console.WriteLine();
+
+string myString = "Hello❤️";
+Console.WriteLine(myString.Length);
+Console.WriteLine();
+
+for (int i = 0; i < myString.Length; i++)
+{
+    Console.WriteLine($"{(int)myString[i]} - {myString[i]} - {char.IsHighSurrogate(myString, i)}");
+}
