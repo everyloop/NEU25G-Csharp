d6c64eedb0df4779544a87ed2add20ce61976e66
Fredrik Johansson <fredrik@everyloop.com>
2025-09-19 11:16:15 +0200

    Add L014

---
+++ Code-alongs/L014_StreamReader_&_StreamWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Console.WriteLine($"Current directory: {Directory.GetCurrentDirectory()}");
+// Directory.SetCurrentDirectory(@"c:\");
+
+//var path = @"C:\Users\Fredrik\source\repos\NEU25G-Csharp\Code-alongs\L013_Path_and_Directory\program.cs";
+
+//using (StreamReader reader = new StreamReader(path))
+//{
+//    // Console.WriteLine(reader.ReadToEnd());
+
+//    while (!reader.EndOfStream)
+//    {
+//        // Console.WriteLine(reader.ReadLine());
+//        Console.Write((char)reader.Read());
+//        Thread.Sleep(50);
+//    }
+//}
+
+using (var writer = new StreamWriter("myFile.txt"))
+{
+    writer.WriteLine("Hello");
+    writer.WriteLine("World");
+
+    for (int i = 65; i < 90; i++)
+    {
+        writer.Write((char)i);
+    }
+
+    writer.WriteLine();
+}
