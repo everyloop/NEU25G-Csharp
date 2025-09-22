9a77aa94b0aec94608ee2a985384e3b7a1f1ac19
Fredrik Johansson <fredrik@everyloop.com>
2025-09-22 16:50:12 +0200

    Code cleanup

--- Code-alongs/L016_OOP_Intro/Program.cs
+++ Code-alongs/L016_OOP_Intro/Program.cs
@@ -1,24 +1,44 @@
 ﻿
-
 Cat myCat = new Cat() { name = "Pelle", age = 4 };
 Cat myOtherCat = new Cat() { name = "Måns", age = 5 };
+Cat myThirdCat = new Cat() { name = "Bill", age = 2 };
+Cat myForthCat = new Cat() { name = "Bull", age = 2 };
 
-myCat.Greet();
-myOtherCat.Greet();
+Cat[] cats = new Cat[] { myCat, myOtherCat, myThirdCat, myForthCat };
 
+//Cat[] cats = new Cat[] {
+//    new Cat() { name = "Pelle", age = 4 },
+//    new Cat() { name = "Måns", age = 5 },
+//    new Cat() { name = "Bill", age = 2 },
+//    new Cat() { name = "Bull", age = 2 },
+//    new Cat() { name = "Maja" },
+//};
+
+Console.WriteLine("*** Looping through the cats, printing fields and running methods ***");
+
+foreach (var cat in cats)
+{
+    // Console.WriteLine($"cat.GetDoubleAge() => {cat.GetDoubleAge()}"); // <= Fungerar inte att anropa eftersom metoden är 'private'.
+
+    Console.WriteLine($"\n{cat.name} är {cat.age} år gammal.");
+    cat.Greet();
+}
+
+Console.WriteLine("\n\n*** Cat greeting Fredrik ***");
 myOtherCat.Greet("Fredrik");
 
-// Console.WriteLine($"myCat.GetDoubleAge() => {myCat.GetDoubleAge()}"); // <= Fungerar inte att anropa eftersom metoden är 'private'.
+Console.WriteLine("\n\n*** Cats greeting each other ***");
+myCat.Greet(myOtherCat); 
+myOtherCat.Greet(myCat);
 
+
+// Statiska medlemmar hör till datatypen, inte de enskilda instanserna.
+
+Console.WriteLine("\n\n*** Static members ***");
 Cat.numberOfLives = 6;
-Console.WriteLine(Cat.numberOfLives);
-
+Console.WriteLine($"Cat.numberOfLives = {Cat.numberOfLives}");
 Cat.PrintNumberOfLives();
 
-Console.WriteLine();
-
-myCat.Greet(myOtherCat); // <= Fixa en overload method av Greet() så att denna raden funkar.
-myOtherCat.Greet(myCat);
 
 // Även klasser kan defineras som static, vilket betyder att den inte kan instansieras alls (och inte heller ha instans(icke statiska)-medlemar)
 // Exempel på sådana klasser är Console och Math. Det är inte meningen att man ska göra instanser av dem, utan de tillhandhåller allmän funktionalitet.
@@ -85,20 +105,3 @@ class Cat
 
 
 
-//Cat myThirdCat = new Cat() { name = "Bill", age = 2 };
-//Cat myForthCat = new Cat() { name = "Bull", age = 2 };
-
-//Cat[] cats = new Cat[] { myCat, myOtherCat, myThirdCat, myForthCat, new Cat() { name = "Maja" } };
-
-//Cat[] cats = new Cat[] {
-//    new Cat() { name = "Pelle", age = 4 },
-//    new Cat() { name = "Måns", age = 5 },
-//    new Cat() { name = "Bill", age = 2 },
-//    new Cat() { name = "Bull", age = 2 },
-//    new Cat() { name = "Maja" },
-//};
-
-//foreach (var cat in cats)
-//{
-//    Console.WriteLine($"{cat.name} är {cat.age} år gammal.");
-//}
