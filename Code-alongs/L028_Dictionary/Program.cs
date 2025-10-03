3c61c29dc9a5082349d503fd6846e24afe1d0e77
Fredrik Johansson <fredrik@everyloop.com>
2025-10-03 11:59:09 +0200

    Add L028_Dictionary

---
+++ Code-alongs/L028_Dictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿
+var myDictionary = new Dictionary<string, string>();
+
+myDictionary.Add("boy", "pojke");
+myDictionary.Add("girl", "flicka");
+myDictionary.Add("man", "man");
+myDictionary.Add("woman", "kvinna");
+
+Console.WriteLine("Keys:");
+
+foreach (string key in myDictionary.Keys)
+{
+    Console.WriteLine(key);
+}
+
+Console.WriteLine("\nValues:");
+
+foreach (string value in myDictionary.Values)
+{
+    Console.WriteLine(value);
+}
+
+Console.WriteLine("\nKeys with values:");
+
+foreach (KeyValuePair<string, string> keyValuePair in myDictionary)
+{
+    Console.WriteLine($"The key {keyValuePair.Key} holds the value {keyValuePair.Value}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"myDictionary[\"girl\"] => {myDictionary["girl"]}");
+
+// Om nyckeln saknas kastas en KeyNotFoundException
+// Console.WriteLine($"myDictionary[\"tree\"] => {myDictionary["tree"]}");
+
+
+string input;
+
+do
+{
+    input = Console.ReadLine();
+
+    if (myDictionary.ContainsKey(input))
+    {
+        Console.WriteLine(myDictionary[input]);
+    }
+    else
+    {
+        Console.WriteLine("Nyckeln saknas.");
+    }
+} while (input != string.Empty);
