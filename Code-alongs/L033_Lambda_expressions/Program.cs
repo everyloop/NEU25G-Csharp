61ed64ab18272d691e52cf8e81cdaba7f71e5262
Fredrik Johansson <fredrik@everyloop.com>
2025-10-08 11:18:19 +0200

    Add L033, L034

---
+++ Code-alongs/L033_Lambda_expressions/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+Func<int, int> square = x => x * x;
+Action<int> printInt = i => Console.WriteLine(i);
+
+printInt(square(5));
+
+//static int Square(int x)
+//{
+//    return x * x;
+//}
+
+PrintResults(x => x * 10 + 5);
+
+Console.WriteLine();
+
+PrintResults(x => x % 3);
+
+Console.WriteLine();
+
+PrintResults(n =>
+{
+	int sum = 0;
+
+	for (int i = 0; i <= n; i++)
+	{
+		sum += i;
+	}
+	return sum;
+});
+
+static void PrintResults(Func<int, int> func)
+{
+	for (int i = 1; i <= 10; i++)
+	{
+        Console.WriteLine($"{i}: {func(i)}");
+	}
+}
+
+Console.WriteLine();
+
+Func<Person, int, bool> isLegal = (person, legalAge) => person.Age >= legalAge;
+Console.WriteLine(isLegal(new Person() { Age = 15}, 18));
+
+Console.WriteLine();
+
+Func<int, int, int, string> getString = (a, b, c) => $"{a} + {b} + {c} = {a + b + c}";
+Console.WriteLine(getString(3, 6, 19));
+
+Console.WriteLine();
+
+Func<double, double, double> getCylinderVolume = (r, h) => r * r * Math.PI * h;
+Console.WriteLine(getCylinderVolume(2, 10));
+
+class Person
+{
+    public int Age { get; set; }
+}
---
+++ Code-alongs/L034_Anonymous_types/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+var person = new { FirstName = "Anders", LastName = "Svensson", Age = 34 };
+
+Console.WriteLine(person.FirstName);
+Console.WriteLine(person.Age);
+
+var typeExamples = new { Float = 3.2f, Double = 5.4, Decimal = 102.3m, Bool = true };
+
+Console.WriteLine();
+
+var countries = new string[] { "Sweden", "Denmark", "Norway" };
+
+var people = new[]
+{
+    new { FirstName = "Anders", LastName = "Svensson", Age = 34 },
+    new { FirstName = "Adam", LastName = "Johansson", Age = 24 },
+    new { FirstName = "Maria", LastName = "Andersson", Age = 42 },
+    new { FirstName = "Karl", LastName = "Karlsson", Age = 19 },
+    new { FirstName = "Tomas", LastName = "Johansson", Age = 21 },
+    new { FirstName = "Sven", LastName = "Palm", Age = 21 },
+    new { FirstName = "Fia", LastName = "Kvist", Age = 28 },
+    new { FirstName = "Camilla", LastName = "Alm", Age = 37 },
+};
+
+foreach (var p in people)
+{
+    Console.WriteLine($"{p.FirstName} {p.LastName} är {p.Age} år gammal.");
+}
+
+var user = new
+{
+    name = "Anders Eriksson",
+    age = 45,
+    contactInfo = new { email = "anders@gmail.com", phone = "2384723984" }
+};
+
+Console.WriteLine(user.contactInfo.email);
