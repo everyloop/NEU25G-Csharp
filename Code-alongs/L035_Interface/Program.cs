fed4b1b45e998ad0c276006ac35d337f83ba87cf
Fredrik Johansson <fredrik@everyloop.com>
2025-10-08 11:58:47 +0200

    Add L035

---
+++ Code-alongs/L035_Interface/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+//using(var reader = new StreamReader("myFile.txt"))
+//{
+//    // ...
+//}
+
+//using (var cat = new Cat())
+//{
+//    Console.WriteLine("Cat");
+//}
+//Console.WriteLine("The end!");
+
+
+var myCat = new Cat("Pelle");
+myCat.Collect();
+
+
+class Animal
+{
+    public string Name { get; set; }
+
+    public Animal(string name)
+    {
+        Name = name;
+    }
+}
+
+class Cat : Animal, IDisposable, ICollectable
+{
+    public Cat(string name) : base(name) { }
+
+    public void Collect()
+    {
+        Console.WriteLine($"{Name} was collected!");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Disposed!");
+    }
+
+    public bool IsCollected()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+interface ICollectable
+{
+    public void Collect();
+    public bool IsCollected();
+}
