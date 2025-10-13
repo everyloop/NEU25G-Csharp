08d22977b5202165dfa8a5e2c8a6206884bc54d8
Fredrik Johansson <fredrik@everyloop.com>
2025-10-13 16:20:06 +0200

    Add comments

--- Code-alongs/L037_Yield_return/Program.cs
+++ Code-alongs/L037_Yield_return/Program.cs
@@ -1,13 +1,40 @@
 ﻿
-foreach (var s in GetStrings())
+//foreach (var s in GetStrings())
+//{
+//    Console.WriteLine(s);
+//}
+
+
+//IEnumerable<string> GetStrings()
+//{
+//    yield return "First";
+//    yield return "Second";
+//    yield return "Third";
+//}
+
+// Skapar en lista med 1 miljard element i minnet, oavsett hur många den anropande koden egentligen använder.
+IEnumerable<int> GenerateWithoutYield()
 {
-    Console.WriteLine(s);
+    var list = new List<int>();
+    for (int i = 0; i < 1000_000_000; i++)
+    {
+        list.Add(i);
+    }
+
+    return list;
 }
 
-
-IEnumerable<string> GetStrings()
+// Returnerar ett värde i taget allt eftersom den anropande koden efterfrågar dem.
+IEnumerable<int> GenerateWithYield()
 {
-    yield return "First";
-    yield return "Second";
-    yield return "Third";
+    for (int i = 0; i < 1000_000_000; i++)
+    {
+        yield return i;
+    }
+}
+
+// Om denna koden istället skulle använda GenerateWithoutYield() så tas mycket onödiga resurser (CPU tid & minne) i anspråk.
+foreach (var i  in GenerateWithYield().Skip(10).Take(5))
+{
+    Console.WriteLine(i);
 }
