dfb403a81eb658ac8f703c9773d3a832ae783088
Fredrik Johansson <fredrik@everyloop.com>
2025-10-13 11:58:19 +0200

    Add L037_Yield_return

---
+++ Code-alongs/L037_Yield_return/Program.cs
@@ -0,0 +1,13 @@
+﻿
+foreach (var s in GetStrings())
+{
+    Console.WriteLine(s);
+}
+
+
+IEnumerable<string> GetStrings()
+{
+    yield return "First";
+    yield return "Second";
+    yield return "Third";
+}
