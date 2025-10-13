bab1cd5bd0b3c99969ed5a94bb6aacf0c2ca5e37
Fredrik Johansson <fredrik@everyloop.com>
2025-10-13 09:50:40 +0200

    Add L036_LINQ

---
+++ Code-alongs/L036_LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+var objectList = new List<object>();
+
+objectList.Add("Hello world!");
+objectList.Add("This is a text");
+objectList.Add(5.3);
+objectList.Add("Hej");
+objectList.Add(true);
+objectList.Add(94);
+objectList.Add('a');
+objectList.Add("Bye!");
+
+//var stringList = new List<string>();
+
+//foreach (var o in objectList)
+//{
+//    if (o is string s) stringList.Add(s);
+//}
+
+Console.WriteLine("objectList:");
+Console.WriteLine(string.Join('\n', objectList));
+
+var stringList = objectList.OfType<string>().ToList();
+
+Console.WriteLine("\n*** OfType - Filters the elements based on a specified type - E.g. OfType<string> ***");
+Console.WriteLine("\nstringList:");
+Console.WriteLine(string.Join('\n', stringList));
+
+
+Console.WriteLine();
+
