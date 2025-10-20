d786466711e2be1865e0ca6e5981f6316162add4
Fredrik Johansson <fredrik@everyloop.com>
2025-10-20 15:06:42 +0200

    Add L038. L039

---
+++ Code-alongs/L038_PLINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+//using System.Diagnostics;
+
+////Process.GetCurrentProcess().ProcessorAffinity = 1;
+
+//var list = Enumerable.Range(1, 50)
+//    .AsParallel()
+//    .Select(i => CostlyOperation(i))
+//    .ToList();
+
+//Console.WriteLine("\nProcessing complete! Here is the result: \n");
+
+//Console.WriteLine(string.Join(", ", list));
+
+
+//static int CostlyOperation(int n)
+//{
+//    //Thread.Sleep(500);
+//    for (int i = 0; i < 1_000_000_000; i++) ;
+
+//    Console.WriteLine($"Processing object {n}.");
+
+//    return n;
+//}
+
+long sum = Enumerable.Range(1, 1_000_000_000)
+    .AsParallel()
+    .WithDegreeOfParallelism(Environment.ProcessorCount / 2)
+    .Where(i => i % 3 == 0 || i % 5 == 0)
+    .Select(i => (long)i)
+    .Sum();
+
+Console.WriteLine(sum);
+Console.WriteLine();
---
+++ Code-alongs/L039_Tasks/Program.cs
@@ -0,0 +1,89 @@
+﻿
+
+
+
+
+Task<long> myTask = new Task<long>(() =>
+{
+    long sum = 0;
+
+	for (int i = 0; i < 1000_000_000; i++)
+	{
+		sum += i;
+	}
+
+	return sum;
+});
+
+Console.WriteLine("Starting the task ...");
+
+myTask.ContinueWith(task =>
+{
+	Console.WriteLine($"Task result: {myTask.Result}");
+});
+
+myTask.Start();
+
+Console.WriteLine("Continues executing code ...");
+
+//myTask.Wait();
+
+Console.WriteLine("Task is complete!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Task myTask = Task.Run(TaskOne);
+
+//Task myTask = new Task(() => TaskMethod("Task A"));
+
+//new Task(() => TaskMethod("Task B")).Start();
+
+//Console.WriteLine($"myTask is {myTask.Status}");
+
+//myTask.Start();
+
+//Console.WriteLine($"myTask is {myTask.Status}");
+
+//for (int i = 0; i < 1000; i++)
+//{
+//    Console.WriteLine($"Main process: {i}");
+//}
+
+//Console.WriteLine($"myTask is {myTask.Status}");
+
+//Console.WriteLine("The end!");
+
+//static void TaskMethod(string identifier)
+//{
+//    for (int i = 0; i < 1000; i++)
+//    {
+//        Console.WriteLine($"{identifier}: {i}");
+//    }
+//}
