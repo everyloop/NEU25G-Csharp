7b86b425a11c3409172cf241dcde3361105a08c6
Fredrik Johansson <fredrik@everyloop.com>
2025-10-23 09:03:02 +0200

    Add L040, L041

---
+++ Code-alongs/L040_Async_Await/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    private static HttpClient client = new HttpClient();
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Program start.");
+
+        Task<int> myTask = Download();
+
+        Console.WriteLine("Main continues ...");
+
+        //for (int i = 0; i < 500; i++)
+        //{
+        //    await Task.Delay(10);
+        //    Console.Write($"{i}, ");
+        //}
+
+        try
+        {
+            int dataLength = await myTask;
+            Console.WriteLine($"Got {dataLength} chars of data.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+
+        Console.WriteLine("Program end.");
+    }
+
+    private static async Task<int> Download()
+    {
+        throw new Exception("Oops!");
+
+        Task<string> download = client.GetStringAsync("http://google.com");
+
+        Console.WriteLine("Download started ...");
+
+        string data = await download;
+
+        Console.WriteLine("Download complete!");
+
+        await Task.Delay(2000);
+
+        Console.WriteLine("Completed unnecessary delay in Download!");
+
+        Console.WriteLine("Start CPU bound operation asynchronously");
+
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 2000_000_000; i++) ;
+        });
+
+        Console.WriteLine("CPU bound operation ended.");
+
+        return data.Length;
+    }
+}
---
+++ Code-alongs/L041_Race_conditions/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+for (int i = 0; i < 20; i++)
+{
+    Count();
+}
+
+static void Count()
+{
+    object myLock = new object();
+
+    int count = 0;
+
+    var taskA = Task.Run(DoCount);
+    var taskB = Task.Run(DoCount);
+    var taskC = Task.Run(DoCount);
+    var taskD = Task.Run(DoCount);
+
+    Task.WaitAll(taskA, taskB, taskC, taskD);
+
+    Console.WriteLine(count);
+
+    void DoCount()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            lock (myLock)
+            {
+                int temp = count;
+                temp = temp + 1;
+                count = temp;
+            }
+        }
+    }
+}
