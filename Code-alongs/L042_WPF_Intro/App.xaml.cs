53f726983dd2483f86a6663cb1c51c8c87d73c12
Fredrik Johansson <fredrik@everyloop.com>
2025-10-24 11:59:50 +0200

    Add L042

---
+++ Code-alongs/L042_WPF_Intro/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace L042_WPF_Intro
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
