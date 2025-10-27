09fe4c9bec89c06e6a4eafbdd685bf317d73f004
Fredrik Johansson <fredrik@everyloop.com>
2025-10-27 13:35:42 +0100

    Add L044

---
+++ Code-alongs/L044_Controls/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace L044_Controls
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Code-alongs/L044_Controls/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L044_Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBox.Text = "Hello world";
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {   
+            if (e.Key == Key.Enter)
+            {
+                label.Content = textBox.Text;
+            }
+        }
+    }
+}
