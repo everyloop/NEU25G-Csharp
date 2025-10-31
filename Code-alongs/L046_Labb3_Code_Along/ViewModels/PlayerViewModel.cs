9ec54d83120a3f3cd6ae98fff819ec9775e3eb56
Fredrik Johansson <fredrik@everyloop.com>
2025-10-31 12:24:52 +0100

    Add example for delegate commands.

--- Code-alongs/L046_Labb3_Code_Along/ViewModels/PlayerViewModel.cs
+++ Code-alongs/L046_Labb3_Code_Along/ViewModels/PlayerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using L046_Labb3_Code_Along.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,38 @@ namespace L046_Labb3_Code_Along.ViewModels
     {
         private readonly MainWindowViewModel? _mainWindowViewModel;
 
+        public DelegateCommand SetPackNameCommand { get; }
         public QuestionPackViewModel? ActivePack { get => _mainWindowViewModel?.ActivePack; }
         public PlayerViewModel(MainWindowViewModel? mainWindowViewModel)
         {
             this._mainWindowViewModel = mainWindowViewModel;
+
+            SetPackNameCommand = new DelegateCommand(SetPackName, CanSetPackName);
+            DemoText = string.Empty;
+        }
+
+        private string _demoText;
+
+        public string DemoText
+        {
+            get { return _demoText; }
+            set 
+            { 
+                _demoText = value;
+                RaisePropertyChanged();
+                SetPackNameCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+
+        private bool CanSetPackName(object? arg)
+        {
+            return DemoText.Length > 0;
+        }
+
+        private void SetPackName(object? obj)
+        {
+            ActivePack.Name = DemoText;
         }
     }
 }
