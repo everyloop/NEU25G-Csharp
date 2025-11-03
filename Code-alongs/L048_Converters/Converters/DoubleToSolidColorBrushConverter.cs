a1c81fdf38eff21144dbd0a8428d80e14b4ef10d
Fredrik Johansson <fredrik@everyloop.com>
2025-11-03 16:16:57 +0100

    Add L048_Converters

---
+++ Code-alongs/L048_Converters/Converters/DoubleToSolidColorBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace L048_Converters.Converters
+{
+    class DoubleToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(Brush))
+            {
+                byte colorValue = (byte)Math.Clamp((double)(value), 0, 255);
+                return new SolidColorBrush(Color.FromRgb(colorValue, colorValue, colorValue));
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
