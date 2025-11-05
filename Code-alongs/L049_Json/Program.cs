c6b8d770f1c252b14dcd02fc7c98597be785efbe
Fredrik Johansson <fredrik@everyloop.com>
2025-11-05 10:54:31 +0100

    Add L049_Json

---
+++ Code-alongs/L049_Json/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+var people = new Person[]
+{
+    new Person() { FirstName = "Anders", LastName = "Andersson", Age = 25 },
+    new Person() { FirstName = "Kalle", LastName = "Svensson", Age = 28 },
+    new Person() { FirstName = "Maria", LastName = "Andersson", Age = 24 },
+};
+
+string json = JsonSerializer.Serialize(people);
+
+Console.WriteLine(json);
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
